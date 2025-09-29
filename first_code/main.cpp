9e7a9eac6da25ab3e2d781cb00f4982d986d3ac7
abhi bauhre <abhishekbohre77@gmail.com>
2025-09-29 14:37:45 +0530

    first code

---
+++ first_code/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+
+    cout<<"enter a number bw 1 to 100";
+    cin>>number;
+    cout<<"wow amazing thats my fav no to" <<endl;
+    cout<<"no really :" <<number << "is my fav number:";
+    return 0;
+}
